2bc9dac30196561ae1d7547f816e0bbddc48aeba
ravipan30 <ravipan30@gmail.com>
2019-09-11 13:41:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cd source-code
   ./mvnw clean package
 cd ..
 
-cp demo_ravi_h2/target/demo_ravi_h2-0.0.1-SNAPSHOT.jar  build-output/.
+cp concourse/target/demo_ravi_h2-0.0.1-SNAPSHOT.jar  build-output/.
