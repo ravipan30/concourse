ffb37ab572b8b10cc906db607a0114b1ca91835a
ravipan30@gmail.com <ravi0807ca>
2019-09-12 16:25:02 +0530

    commited test cases

--- src/test/java/com/example/demo_ravi_h2/DemoRaviH2ApplicationTests.java
+++ src/test/java/com/example/demo_ravi_h2/DemoRaviH2ApplicationTests.java
@@ -4,13 +4,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+		import org.junit.runner.RunWith;
+
+		import org.springframework.beans.factory.annotation.Autowired;
+		import org.springframework.boot.test.context.SpringBootTest;
+		import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+		import org.springframework.boot.test.web.client.TestRestTemplate;
+		import org.springframework.boot.web.server.LocalServerPort;
+		import org.springframework.test.context.junit4.SpringRunner;
+
+		import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoRaviH2ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Hi This is sample project");
+	}
 }
